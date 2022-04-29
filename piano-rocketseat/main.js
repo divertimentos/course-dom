b321d9597cc362a3072c3732968a74db39199cea
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2022-04-29 09:22:34 -0300

    First Commit | Adds initial files, and .gitignore

---
+++ piano-rocketseat/main.js
@@ -0,0 +1,11 @@
+// get all keys
+//const keys = document.querySelector
+//const keys = document.getElement
+//const keys = document.getEleme
+const keys = document.getElementById("key")
+
+// play notes
+
+// click with mouse
+
+// keyboard type
