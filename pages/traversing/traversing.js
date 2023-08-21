569c8ad3f3dae15aff95cc06e4e778046d92b4f2
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2023-08-21 16:19:02 -0300

    Removes unneeded code

--- pages/traversing/traversing.js
+++ pages/traversing/traversing.js
@@ -9,18 +9,11 @@ descendant.parentNode.style.backgroundColor = "#F2B4B8";
 
 let joke = document.querySelector(".joke");
 let sibling = joke.nextSibling.nextSibling;
-sibling.style.border = "5px solid cornflowerblue";
+sibling.style.border = "5px solid firebrick";
 
 let bodyChildren = document.body.children;
 
 for (let i = 0; i < bodyChildren.length; i++) {
   let child = bodyChildren[i];
-  console.log("child:");
-  console.log(child);
-  // child.style.color = "#4a4e69";
   child.style.backgroundColor = "#9CC0EB";
 }
-
-let h1 = document.getElementsByTagName("h1");
-// console.log(h1[0].firstChild); // returns the text inside <h1 />
-// console.log(h1[0].firstElementChild); // returns null because you anted a child that is an element;
