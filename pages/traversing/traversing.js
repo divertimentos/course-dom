9e791ce02b9b194f59c90fa64dc598f8cfb55480
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2023-08-21 14:11:24 -0300

    Siblings

--- pages/traversing/traversing.js
+++ pages/traversing/traversing.js
@@ -1,2 +1,9 @@
-document.getElementById("wrapper").parentNode.parentNode.style.backgroundColor =
-  "cornflowerblue";
+let div = document.getElementsByTagName("div")[0];
+
+let ancestor = div.parentNode;
+ancestor.style.backgroundColor = "cornflowerblue";
+
+let joke = document.querySelector(".joke");
+let sibling = joke.nextSibling.nextSibling;
+sibling.style.border = "5px solid firebrick";
+console.log(sibling);
